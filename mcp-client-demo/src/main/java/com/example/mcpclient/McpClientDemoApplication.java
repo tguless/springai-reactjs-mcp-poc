8babb675208b27e7a834f1eedae6aee5d349996a
Ted Gulesserian <ted@pomsoft.net>
2025-04-10 16:39:23 -0400

    Azure OpenAI

--- mcp-client-demo/src/main/java/com/example/mcpclient/McpClientDemoApplication.java
+++ mcp-client-demo/src/main/java/com/example/mcpclient/McpClientDemoApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(scanBasePackages = {"com.example.common", "com.example.mcpclient"})
-@EnableFeignClients(basePackages = {"com.example.common", "com.example.mcpclient"})
+@SpringBootApplication(scanBasePackages = {"com.example.common", "com.example.mcpclient", "com.example.duffel"})
+@EnableFeignClients(basePackages = {"com.example.common", "com.example.mcpclient",  "com.example.duffel"})
 public class McpClientDemoApplication {
 
     public static void main(String[] args) {
